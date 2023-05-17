10a8afdb85d4f9f3cb38c993157f4071f8691a2c
Mohammed Naji <moha.buy@gmail.com>
2023-05-17 23:47:31 +0400

    undo changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component, Suspense } from 'react'
 import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
 import './scss/style.scss'
-import userStore from "./store/user";
-import user from "./store/user";
-import PrivacyPolicyPage from "./views/frontend/PrivacyPolicyPage";
-import HomePage from "./views/frontend/HomePage";
+import userStore from './store/user'
+import user from './store/user'
+import PrivacyPolicyPage from './views/frontend/PrivacyPolicyPage'
+import HomePage from './views/frontend/HomePage'
 
 const loading = (
   <div className="pt-3 text-center">
@@ -21,23 +21,23 @@ const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 
 class App extends Component {
-
   render() {
     return (
       <BrowserRouter>
         <Suspense fallback={loading}>
           <Routes>
+            <Route exact path="/" name="Home" element={<HomePage />} />
+            <Route
+              path="/privacy"
+              name="Yaabaay - Privacy Policy"
+              element={<PrivacyPolicyPage />}
+            />
 
-          <Route path="/" name="Home" element={<HomePage />} />
-          <Route path="/admin" name="Dashboard" element={<Login />} />
-          <Route path="/privacy" name="Yaabaay - Privacy Policy" element={<PrivacyPolicyPage />} />
-
-          {/*<Route exact path="/register" name="Register Page" element={<Register />} />*/}
-          <Route exact path="/404" name="Page 404" element={<Page404 />} />
-          <Route exact path="/500" name="Page 500" element={<Page500 />} />
-
-          <Route path="*" name="Home" element={<DefaultLayout />} />
-
+            {/*<Route exact path="/register" name="Register Page" element={<Register />} />*/}
+            <Route exact path="/404" name="Page 404" element={<Page404 />} />
+            <Route exact path="/500" name="Page 500" element={<Page500 />} />
+            <Route path="/admin" name="Dashboard" element={<Login />} />
+            <Route path="*" name="Home" element={<DefaultLayout />} />
           </Routes>
         </Suspense>
       </BrowserRouter>
